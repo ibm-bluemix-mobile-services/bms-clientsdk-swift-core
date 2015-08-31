c36d30fb8946349972202a826f0ab8a9b1f6d132
Anthony Oliveri <oliveri@us.ibm.com>
2015-08-31 15:15:43 -0500

    Created empty Xcode Swift project

---
+++ BMSCore/BMSCore.h
@@ -0,0 +1,19 @@
+//
+//  BMSCore.h
+//  BMSCore
+//
+//  Created by Anthony Oliveri on 8/31/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMSCore.
+FOUNDATION_EXPORT double BMSCoreVersionNumber;
+
+//! Project version string for BMSCore.
+FOUNDATION_EXPORT const unsigned char BMSCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSCore/PublicHeader.h>
+
+
