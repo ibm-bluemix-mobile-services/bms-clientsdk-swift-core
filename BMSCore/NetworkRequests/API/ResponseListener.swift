055ce8136249528bf18d4848703f22c2abf2fe3e
Anthony Oliveri <oliveri@us.ibm.com>
2015-09-02 12:05:58 -0500

    Full implementation of ResponseListener

--- BMSCore/NetworkRequests/API/ResponseListener.swift
+++ BMSCore/NetworkRequests/API/ResponseListener.swift
@@ -9,6 +9,27 @@
 import Foundation
 
 
+/**
+*  ResponseListener is the interface that will be called after the ResourceRequest has completed or failed.
+*/
+
 public protocol ResponseListener {
+    
+    /**
+    *  This method will be called only when a response from the server has been received with an http status
+    *  in the 200 range.
+    *  @param response The server response
+    */
+    func onSuccess (response: Response)
+    
+    /**
+    *  This method will be called either when there is no response from the server or when the status
+    *  from the server response is in the 400 or 500 ranges. The FailResponse contains an error code
+    *  distinguishing between the different cases.
+    *
+    *  @param response Contains detail regarding why the request failed
+    *  @param error Error that could have caused the request to fail. Null if no error is thrown.
+    */
+    func onFailure (response: FailResponse, error: NSError)
     
 }
