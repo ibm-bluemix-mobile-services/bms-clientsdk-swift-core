e366d5ef31fb57acf1c4771d7236cfca26d6f213
Anthony Oliveri <oliveri@us.ibm.com>
2015-10-05 15:43:17 -0500

    Add targets for a WatchOS 2 and an iOS test app

---
+++ TestAppWatchOS Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TestAppWatchOS Extension
+//
+//  Created by Anthony Oliveri on 10/5/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
