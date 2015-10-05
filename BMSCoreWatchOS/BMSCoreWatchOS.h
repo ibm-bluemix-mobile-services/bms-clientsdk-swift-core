d66454303fff8fc37fc6aebed29a747e2f6fc3d8
Anthony Oliveri <oliveri@us.ibm.com>
2015-10-05 15:24:37 -0500

    Add target for WatchOS framework

---
+++ BMSCoreWatchOS/BMSCoreWatchOS.h
@@ -0,0 +1,19 @@
+//
+//  BMSCoreWatchOS.h
+//  BMSCoreWatchOS
+//
+//  Created by Anthony Oliveri on 10/2/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BMSCoreWatchOS.
+FOUNDATION_EXPORT double BMSCoreWatchOSVersionNumber;
+
+//! Project version string for BMSCoreWatchOS.
+FOUNDATION_EXPORT const unsigned char BMSCoreWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSCoreWatchOS/PublicHeader.h>
+
+
