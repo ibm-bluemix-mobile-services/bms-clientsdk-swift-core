a1f675f4f8d833eee76752aadd37bed5a08894cd
Nana Amfo <noamfo@us.ibm.com>
2015-11-25 14:11:48 -0600

    add a few more unit tests (overflow, buffer files, getLogs...)

--- BMSCoreTests/LoggerTests.swift
+++ BMSCoreTests/LoggerTests.swift
@@ -51,26 +51,6 @@ class LoggerTests: XCTestCase {
         XCTAssertFalse(capture2)
     }
     
-//    func testGetFilesForLogLevel(){
-//        let fakePKG = "MYPKG"
-//        let pathToFile = Logger.logsDocumentPath + FILE_LOGGER_LOGS
-//        let pathToOverflowFile = Logger.logsDocumentPath + FILE_LOGGER_OVERFLOW
-//        
-//
-//        do {
-//            try NSFileManager().removeItemAtPath(pathToFile)
-//        } catch {
-//            print("Could not delete " + pathToFile)
-//        }
-//
-//        let loggerInstance = Logger.getLoggerForName(fakePKG)
-//
-//        let (logFile, logOverflowFile, fileDispatchQueue) = loggerInstance.getFilesForLogLevel(LogLevel.Debug)
-//        
-//        XCTAssertTrue(logFile == path
-//        
-//    }
-    
     func testGetFilesForLogLevel(){
         let fakePKG = "MYPKG"
         let pathToLoggerFile = Logger.logsDocumentPath + FILE_LOGGER_LOGS
@@ -372,6 +352,291 @@ class LoggerTests: XCTestCase {
         XCTAssertTrue(exceptionMessage[TAG_TIMESTAMP] != nil)
         XCTAssertTrue(exceptionMessage[TAG_LEVEL] == "FATAL")
         
+    }
+    
+    func testGetLogs(){
+        let fakePKG = "MYPKG"
+        let pathToFile = Logger.logsDocumentPath + FILE_LOGGER_LOGS
+        let pathToBuffer = Logger.logsDocumentPath + FILE_LOGGER_SEND
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToFile)
+          
+        } catch {
+            
+        }
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToBuffer)
+            
+        } catch {
+            
+        }
+        
+        let loggerInstance = Logger.getLoggerForName(fakePKG)
+        Logger.logStoreEnabled = true
+        Logger.logLevelFilter = LogLevel.Debug
+        Logger.maxLogStoreSize = DEFAULT_MAX_STORE_SIZE
+        
+        loggerInstance.debug("Hello world")
+        loggerInstance.info("1242342342343243242342")
+        loggerInstance.warn("Str: heyoooooo")
+        loggerInstance.error("1 2 3 4")
+        loggerInstance.fatal("StephenColbert")
+        
+        let logs: String! =  try! Logger.getLogs(fileName: FILE_LOGGER_LOGS, overflowFileName: FILE_LOGGER_OVERFLOW, bufferFileName: FILE_LOGGER_SEND)
+        
+        let bufferFile = NSFileManager().fileExistsAtPath(pathToBuffer)
+        
+        XCTAssertTrue(bufferFile)
+        
+        let fileContents = "[\(logs)]"
+        
+        let logDict : NSData = fileContents.dataUsingEncoding(NSUTF8StringEncoding)!
+        let jsonDict: AnyObject? = try! NSJSONSerialization.JSONObjectWithData(logDict, options:NSJSONReadingOptions.MutableContainers)
+        
+        let debugMessage = jsonDict![0]
+        XCTAssertTrue(debugMessage[TAG_MSG] == "Hello world")
+        XCTAssertTrue(debugMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(debugMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(debugMessage[TAG_LEVEL] == "DEBUG")
+        
+        let infoMessage = jsonDict![1]
+        XCTAssertTrue(infoMessage[TAG_MSG] == "1242342342343243242342")
+        XCTAssertTrue(infoMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(infoMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(infoMessage[TAG_LEVEL] == "INFO")
+        
+        let warnMessage = jsonDict![2]
+        XCTAssertTrue(warnMessage[TAG_MSG] == "Str: heyoooooo")
+        XCTAssertTrue(warnMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(warnMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(warnMessage[TAG_LEVEL] == "WARN")
+        
+        let errorMessage = jsonDict![3]
+        XCTAssertTrue(errorMessage[TAG_MSG] == "1 2 3 4")
+        XCTAssertTrue(errorMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(errorMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(errorMessage[TAG_LEVEL] == "ERROR")
+        
+        let fatalMessage = jsonDict![4]
+        XCTAssertTrue(fatalMessage[TAG_MSG] == "StephenColbert")
+        XCTAssertTrue(fatalMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(fatalMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(fatalMessage[TAG_LEVEL] == "FATAL")
+        
+    }
+    
+    func testGetLogWithAnalytics(){
+        let fakePKG = "MYPKG"
+        let pathToFile = Logger.logsDocumentPath + FILE_ANALYTICS_LOGS
+        let pathToBuffer = Logger.logsDocumentPath + FILE_ANALYTICS_SEND
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToFile)
+            
+        } catch {
+            
+        }
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToBuffer)
+            
+        } catch {
+            
+        }
+        
+        let loggerInstance = Logger.getLoggerForName(fakePKG)
+        Analytics.enabled = true
+        Logger.logLevelFilter = LogLevel.Debug
+        Logger.maxLogStoreSize = DEFAULT_MAX_STORE_SIZE
+        let meta = ["hello": 1]
+        
+        loggerInstance.analytics(meta)
+        
+        let logs: String! =  try! Logger.getLogs(fileName: FILE_ANALYTICS_LOGS, overflowFileName: FILE_ANALYTICS_OVERFLOW, bufferFileName: FILE_ANALYTICS_SEND)
+        
+        let bufferFile = NSFileManager().fileExistsAtPath(pathToBuffer)
+        
+        XCTAssertTrue(bufferFile)
+        
+        let fileContents = "[\(logs)]"
+        
+        let logDict : NSData = fileContents.dataUsingEncoding(NSUTF8StringEncoding)!
+        let jsonDict: AnyObject? = try! NSJSONSerialization.JSONObjectWithData(logDict, options:NSJSONReadingOptions.MutableContainers)
+        
+        let analyticsMessage = jsonDict![0]
+        XCTAssertTrue(analyticsMessage[TAG_MSG] == "")
+        XCTAssertTrue(analyticsMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(analyticsMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(analyticsMessage[TAG_LEVEL] == "ANALYTICS")
+        XCTAssertTrue(analyticsMessage[TAG_META_DATA] == meta)
+
+    }
+    
+    func testOverFlowLogging(){
+        let fakePKG = "MYPKG"
+        let pathToFile = Logger.logsDocumentPath + FILE_LOGGER_LOGS
+        let pathToOverflow = Logger.logsDocumentPath + FILE_LOGGER_OVERFLOW
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToFile)
+            
+        } catch {
+            
+        }
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let path = bundle.pathForResource("largeData", ofType: "txt")
+        let largeData = try! String(contentsOfFile: path!)
+        
+        let loggerInstance = Logger.getLoggerForName(fakePKG)
+        Logger.logStoreEnabled = true
+        Logger.internalSDKLoggingEnabled = false
+        Logger.logLevelFilter = LogLevel.Debug
+        Logger.maxLogStoreSize = DEFAULT_MAX_STORE_SIZE
+        
+        loggerInstance.debug(largeData)
+        loggerInstance.info("1242342342343243242342")
+        loggerInstance.warn("Str: heyoooooo")
+        loggerInstance.error("1 2 3 4")
+        loggerInstance.fatal("StephenColbert")
+        
+        
+        let overflowFile = NSFileManager().fileExistsAtPath(pathToOverflow)
+        
+        XCTAssertTrue(overflowFile)
+        
+        var formattedContents = try! String(contentsOfFile: pathToFile, encoding: NSUTF8StringEncoding)
+        var fileContents = "[\(formattedContents)]"
+        var logDict : NSData = fileContents.dataUsingEncoding(NSUTF8StringEncoding)!
+        var jsonDict: AnyObject? = try! NSJSONSerialization.JSONObjectWithData(logDict, options:NSJSONReadingOptions.MutableContainers)
+        
+        let infoMessage = jsonDict![0]
+        XCTAssertTrue(infoMessage[TAG_MSG] == "1242342342343243242342")
+        XCTAssertTrue(infoMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(infoMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(infoMessage[TAG_LEVEL] == "INFO")
+        
+        let warnMessage = jsonDict![1]
+        XCTAssertTrue(warnMessage[TAG_MSG] == "Str: heyoooooo")
+        XCTAssertTrue(warnMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(warnMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(warnMessage[TAG_LEVEL] == "WARN")
+        
+        let errorMessage = jsonDict![2]
+        XCTAssertTrue(errorMessage[TAG_MSG] == "1 2 3 4")
+        XCTAssertTrue(errorMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(errorMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(errorMessage[TAG_LEVEL] == "ERROR")
+        
+        let fatalMessage = jsonDict![3]
+        XCTAssertTrue(fatalMessage[TAG_MSG] == "StephenColbert")
+        XCTAssertTrue(fatalMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(fatalMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(fatalMessage[TAG_LEVEL] == "FATAL")
+        
+        formattedContents = try! String(contentsOfFile: pathToOverflow, encoding: NSUTF8StringEncoding)
+        fileContents = "[\(formattedContents)]"
+        logDict  = fileContents.dataUsingEncoding(NSUTF8StringEncoding)!
+        jsonDict = try! NSJSONSerialization.JSONObjectWithData(logDict, options:NSJSONReadingOptions.MutableContainers)
+        
+        
+        let overflowMessage = jsonDict![0]
+        XCTAssertTrue(overflowMessage[TAG_MSG] == largeData)
+        XCTAssertTrue(overflowMessage[TAG_PKG] == fakePKG)
+        XCTAssertTrue(overflowMessage[TAG_TIMESTAMP] != nil)
+        XCTAssertTrue(overflowMessage[TAG_LEVEL] == "DEBUG")
+        
+    }
+//    
+//    func testFailOverflowLogging(){
+//        let fakePKG = "MYPKG"
+//        let pathToFile = Logger.logsDocumentPath + FILE_LOGGER_LOGS
+//        let pathToOverflow = Logger.logsDocumentPath + FILE_LOGGER_OVERFLOW
+//        
+//        do {
+//            try NSFileManager().removeItemAtPath(pathToFile)
+//            
+//        } catch {
+//            
+//        }
+//        
+//        let bundle = NSBundle(forClass: self.dynamicType)
+//        let path = bundle.pathForResource("largeData", ofType: "txt")
+//        let largeData = try! String(contentsOfFile: path!)
+//        
+//        let loggerInstance = Logger.getLoggerForName(fakePKG)
+//        Logger.logStoreEnabled = true
+//        Logger.internalSDKLoggingEnabled = false
+//        Logger.logLevelFilter = LogLevel.Debug
+//        Logger.maxLogStoreSize = DEFAULT_MAX_STORE_SIZE
+//        
+//        loggerInstance.debug(largeData)
+//        loggerInstance.info("1242342342343243242342")
+//        loggerInstance.warn("Str: heyoooooo")
+//        loggerInstance.error("1 2 3 4")
+//        loggerInstance.fatal("StephenColbert")
+//        
+//        
+//        let overflowFile = NSFileManager().fileExistsAtPath(pathToOverflow)
+//        
+//        XCTAssertTrue(overflowFile)
+//        
+//        let permission: Int16 = 0o744
+//        
+//        let attributes: [String:AnyObject] = [NSFilePosixPermissions: NSNumber(short: permission)]
+//        try! NSFileManager().setAttributes(attributes, ofItemAtPath: pathToFile)
+//        
+//        loggerInstance.debug(largeData)
+//        
+//
+//        
+//        
+//        
+//    }
+    
+    func testDeleteBufferFile(){
+        let fakePKG = "MYPKG"
+        let pathToFile = Logger.logsDocumentPath + FILE_LOGGER_LOGS
+        let pathToBuffer = Logger.logsDocumentPath + FILE_LOGGER_SEND
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToFile)
+            
+        } catch {
+            
+        }
+        
+        do {
+            try NSFileManager().removeItemAtPath(pathToBuffer)
+            
+        } catch {
+            
+        }
+        
+        let loggerInstance = Logger.getLoggerForName(fakePKG)
+        Logger.logStoreEnabled = true
+        Logger.logLevelFilter = LogLevel.Debug
+        Logger.maxLogStoreSize = DEFAULT_MAX_STORE_SIZE
+        
+        loggerInstance.debug("Hello world")
+        loggerInstance.info("1242342342343243242342")
+        loggerInstance.warn("Str: heyoooooo")
+        loggerInstance.error("1 2 3 4")
+        loggerInstance.fatal("StephenColbert")
+        
+        try! Logger.getLogs(fileName: FILE_LOGGER_LOGS, overflowFileName: FILE_LOGGER_OVERFLOW, bufferFileName: FILE_LOGGER_SEND)
+        
+        var bufferFile = NSFileManager().fileExistsAtPath(pathToBuffer)
+        
+        XCTAssertTrue(bufferFile)
+        
+        Logger.deleteBufferFile(pathToBuffer)
+        
+        bufferFile = NSFileManager().fileExistsAtPath(pathToBuffer)
+        
+        XCTAssertFalse(bufferFile)
     }
 
 //    
