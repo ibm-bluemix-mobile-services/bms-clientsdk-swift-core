a2fe4d4b5329fedbae7bec3f1c778161c143a15f
Anthony Oliveri <oliveri@us.ibm.com>
2016-01-27 10:27:51 -0600

    Updated the host name for sending client logs

--- Source/Constants.swift
+++ Source/Constants.swift
@@ -49,7 +49,7 @@ internal let TAG_MAX_STORE_SIZE = "loggerMaxFileSize"
 internal let DEFAULT_MAX_STORE_SIZE: UInt64 = 100000
 internal let DEFAULT_LOW_BOUND_FILE_SIZE = 10000
 
-internal let HOST_NAME = "mfp-analytics-service"
+internal let HOST_NAME = "mobile-analytics-dashboard"
 internal let UPLOAD_PATH = "/analytics-service/data/events/clientlogs/"
 
 internal let CONTENT_TYPE = "Content-Type"
